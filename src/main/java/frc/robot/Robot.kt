916841839bfb65f0a94bb1b2a0ea75e4a7449cf5
John Bonjour <whitewolfzhang@gmail.com>
2023-11-28 10:20:23 -0800

    first push

---
+++ src/main/java/frc/robot/Robot.kt
@@ -0,0 +1,33 @@
+package frc.robot
+
+import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj2.command.CommandScheduler
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
+
+/**
+ * The VM is configured to automatically run this object (which basically functions as a singleton class),
+ * and to call the functions corresponding to each mode, as described in the TimedRobot documentation.
+ * This is written as an object rather than a class since there should only ever be a single instance, and
+ * it cannot take any constructor arguments. This makes it a natural fit to be an object in Kotlin.
+ *
+ * If you change the name of this object or its package after creating this project, you must also update
+ * the `Main.kt` file in the project. (If you use the IDE's Rename or Move refactorings when renaming the
+ * object or package, it will get changed everywhere.)
+ */
+object Robot : TimedRobot()
+{
+    private val teleopCommands = ParallelCommandGroup()
+
+    override fun robotPeriodic() {
+        CommandScheduler.getInstance().run()
+    }
+
+    override fun teleopInit() {
+        CommandScheduler.getInstance().schedule(teleopCommands)
+    }
+    override fun teleopExit() {
+        CommandScheduler.getInstance().cancel(teleopCommands)
+    }
+}
---
+++ src/main/java/frc/robot/subsystem/VisionSystem.kt
@@ -0,0 +1,35 @@
+package frc.robot.subsystem
+
+import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+object VisionSystem: SubsystemBase() {
+    private val table = NetworkTableInstance.getDefault().getTable("limelight")
+
+    val tx get(): Double = table.getEntry("tx").getDouble(0.0)
+    val ty get(): Double = table.getEntry("ty").getDouble(0.0)
+    val ta get(): Double = table.getEntry("ta").getDouble(0.0)
+    val tv get(): Boolean = table.getEntry("tv").getDouble(0.0) == 1.0
+
+    private data class PowerRange(val power: Double, val minD: Double, val maxD: Double)
+    private val ranges: Array<PowerRange> = arrayOf(
+
+    )
+
+    val currentPower: Double get() {
+        for (range in ranges) {
+            if (ty in range.minD..range.maxD)
+                return range.power
+        }
+
+        return 0.0
+    }
+
+    override fun periodic() {
+        SmartDashboard.putNumber("VisionSystem: width", tx)
+        SmartDashboard.putNumber("VisionSystem: height", ty)
+        SmartDashboard.putNumber("VisionSystem: area", ta)
+        SmartDashboard.putBoolean("VisionSystem: isValid", tv)
+    }
+}
